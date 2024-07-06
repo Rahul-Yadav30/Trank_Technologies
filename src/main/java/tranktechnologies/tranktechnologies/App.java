0c90724c0971f401da92130c590ecffe9ac5aad1
Rahul-Yadav30 <ruyadav30@gmail.com>
2024-07-06 12:45:46 +0530

    trank

---
+++ src/main/java/tranktechnologies/tranktechnologies/App.java
@@ -0,0 +1,13 @@
+package tranktechnologies.tranktechnologies;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
